6383d3c307db0e39aa181ad3fb5a6fa66a5a5eb7
Roman Matsypula <ukrmatsypula@gmail.com>
2022-03-12 23:19:15 +0200

    add new component

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="root">
+    <input
+      type="email"
+      :value="email"
+      @input="emailInput($event.target.value)"
+    />
+    <input
+      type="text"
+      :value="password"
+      @input="passwordValue($event.target.value)"
+    />
+
+    email: {{ email }} password: {{ password }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data: () => ({
+    email: "HelloWorld@gmail.com",
+    password: "pass",
+  }),
+  methods: {
+    emailInput(value) {
+      this.email = value;
+    },
+    passwordValue(passwordValue) {
+      this.password = passwordValue;
+    },
+  },
+};
+</script>
