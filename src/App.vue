afbd1c215c7809343644cc6ec237b68aaf8c1a5f
Roman Matsypula <ukrmatsypula@gmail.com>
2022-03-12 23:38:52 +0200

    add default logic

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <My-component />
+    <Counter />
   </div>
 </template>
 
 <script>
 import MyComponent from "@/components/MyComponent.vue";
+import Counter from '@/components/Counter.vue';
 
 export default {
   name: "App",
   components: {
     MyComponent,
+    Counter,
   },
 };
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="counter">
+    <div class="minus" @click="minus">-</div>
+    <input type="number" :value="counter" />
+    <div class="plus" @click="plus">+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data: () => ({
+    counter: 1 ,
+  }),
+  methods: {
+    minus() {
+      if (this.counter <= 1) {
+        return;
+      }
+      this.counter--;
+    },
+    plus() {
+      if(this.counter > 9){
+        return;
+      }
+      this.counter++
+    },
+  },
+};
+</script>
+
+<style scoped>
+.minus, .plus {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 1px solid #ccc;
+  text-align: center;
+  cursor: pointer;
+  user-select: none;
+}
+</style>
